25d34f98ab25e2b6ebddd5e647267733ceaa7b28
Neel Kukreti <neel.kukreti23@gmail.com>
2020-04-12 23:07:53 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -3,14 +3,14 @@ import Firebase from "firebase";
 
 const firebaseConfig = {
         
-    apiKey: "AIzaSyAJ8ovAPywQFoiiHaDl16-_1pEQgDgOTE8",
-    authDomain: "covidtracker-a742b.firebaseapp.com",
-    databaseURL: 'https://covidtracker-a742b.firebaseio.com',
-    projectId: "covidtracker-a742b",
-    storageBucket: "covidtracker-a742b.appspot.com",
-    messagingSenderId: "898486610796",
-    appId: "1:898486610796:web:1977d0cfd795e9571be149",
-    measurementId: "G-F88ESL18TZ"
+    apiKey: "asd",
+    authDomain: "covidtracasdapp.com",
+    databaseURL: 'https://asdsdbaseio.com',
+    projectId: "covidtasd742b",
+    storageBucket: "covidtrasdspot.com",
+    messagingSenderId: "8984asd6",
+    appId: "1:898486asd1be149",
+    measurementId: "G-F8asdZ"
 
 };
 const app = Firebase.initializeApp(firebaseConfig);
